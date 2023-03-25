0715a8a12dc98be19d1677857f992ae64184d317
artir <artir@REYANSH2021>
2023-03-25 18:18:29 +0530

    Add project files.

---
+++ WebApplication1/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FirstTask.Models;
+
+public partial class Employee
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Gender { get; set; }
+}
---
+++ WebApplication1/Models/MvcdemoContext.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstTask.Models;
+
+public partial class MvcdemoContext : DbContext
+{
+    public MvcdemoContext()
+    {
+    }
+
+    public MvcdemoContext(DbContextOptions<MvcdemoContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Department> Departments { get; set; }
+
+    public virtual DbSet<Doctor> Doctors { get; set; }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("name=FirstTask");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Department>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Departme__3214EC0795668021");
+
+            entity.ToTable("Department");
+
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Doctor>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Doctor__3214EC07543397E6");
+
+            entity.ToTable("Doctor");
+
+            entity.Property(e => e.DEgree)
+                .HasMaxLength(10)
+                .IsUnicode(false)
+                .HasColumnName("dEGREE");
+            entity.Property(e => e.NAme)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("nAME");
+        });
+
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Employee__3214EC076AAEF91B");
+
+            entity.ToTable("Employee");
+
+            entity.Property(e => e.Gender)
+                .HasMaxLength(10)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Student__3214EC074C2D06E8");
+
+            entity.ToTable("Student");
+
+            entity.Property(e => e.Gender)
+                .HasMaxLength(10)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
